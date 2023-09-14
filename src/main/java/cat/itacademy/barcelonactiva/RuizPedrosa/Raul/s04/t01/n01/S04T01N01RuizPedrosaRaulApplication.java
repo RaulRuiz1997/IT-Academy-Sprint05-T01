604d7ad84d02291a2fadc610ffa49129baa49859
Raúl Ruiz <raul.ruizpedrosa@Outlook.com>
2023-09-14 17:26:21 +0200

    Primer commit

---
+++ src/main/java/cat/itacademy/barcelonactiva/RuizPedrosa/Raul/s04/t01/n01/S04T01N01RuizPedrosaRaulApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.RuizPedrosa.Raul.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01RuizPedrosaRaulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01RuizPedrosaRaulApplication.class, args);
+	}
+
+}
